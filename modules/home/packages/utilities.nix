2973158621fc26a3bb0362889f69389d538393eb
Hank Lee <hank.lee.qed@gmail.com>
2025-06-28 20:11:35 -0400

    add utility ls-lint

--- modules/home/packages/utilities.nix
+++ modules/home/packages/utilities.nix
@@ -8,7 +8,7 @@
     wget
     tree
     less # Pager for man pages
-    
+
     # File management and search
     ripgrep # Fast grep (rg)
     fd # Fast find
@@ -16,23 +16,24 @@
     eza # Better ls
     bat # Better cat with syntax highlighting
     hexyl # Hex viewer
-    
+    ls-lint
+
     # File operations
     ouch # Compression/decompression
     p7zip # 7-zip archives
-    
+
     # System monitoring
     dust # Disk usage analyzer
     procs # Better ps
     lnav # Log file navigator
-    
+
     # Documentation
     tlrc # tldr client
-    
+
     # Version control
     git-extras
     git-filter-repo
-    
+
     # Other CLI tools
     omnix # Nix tooling
   ];
